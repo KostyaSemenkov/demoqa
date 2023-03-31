a96ada5861532e6d0aef15bf3b15a4a49043d873
Semenkov_Kostya <koldcoed@mail.ru>
2023-03-31 20:35:14 +0300

    1

--- tests/test_attribute.py
+++ tests/test_attribute.py
@@ -1,7 +1,13 @@
 from pages.text_box import TextBox
+import allure
 
 
 def test_attribute_holder(browser):
     holder = TextBox(browser)
     holder.visit()
     assert holder.first_pole.get_dom_attribute('placeholder') == 'Full Name'
+
+
+@allure.feature('check attr')
+def test_placeholder(browser):
+    pass
